65be89af41be4170d74089218be06b7bc14d5fe8
DRAGKILLS <70339357+DRAGKILLS@users.noreply.github.com>
2021-08-16 08:30:52 -0700

    Update SignCommand.php

--- src/Fludixx/Bedwars/command/SignCommand.php
+++ src/Fludixx/Bedwars/command/SignCommand.php
@@ -17,33 +17,33 @@ use pocketmine\Player;
 
 class SignCommand extends Command {
 
-	public function __construct()
-	{
-		parent::__construct("sign",
-			"/sign [ARENANAME]",
-			"/sign [ARENANAME]", ['addsign']);
-	}
+    public function __construct()
+    {
+        parent::__construct("sign",
+            "/sign [ARENANAME]",
+            "/sign [ARENANAME]", ['addsign']);
+    }
 
-	public function execute(CommandSender $sender, string $commandLabel, array $args)
-	{
-		if($sender->hasPermission("bw.admin") and $sender instanceof Player) {
-			if(!isset($args[0]) or $args[0] == "help") {
-				$sender->sendMessage(Bedwars::PREFIX."/sign [ARENANAME]");
-				return TRUE;
-			} else {
-				$arenas = Bedwars::$provider->getArenas();
-				if(!isset($arenas[$args[0]])) {
-					$sender->sendMessage(Bedwars::PREFIX."Arena not found! Be sure to register it");
-					return FALSE;
-				} else {
-					$sender->sendMessage(Bedwars::PREFIX."Break a Sign");
-					Bedwars::$players[$sender->getName()]->setPos(-11);
-					Bedwars::$players[$sender->getName()]->setKnocker($args[0]);
-					return TRUE;
-				}
-			}
-		}
-		return FALSE;
-	}
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        if($sender->hasPermission("bw.admin") and $sender instanceof Player) {
+            if(!isset($args[0]) or $args[0] == "help") {
+                $sender->sendMessage(Bedwars::PREFIX."/sign [ARENANAME]");
+                return TRUE;
+            } else {
+                $arenas = Bedwars::$provider->getArenas();
+                if(!isset($arenas[$args[0]])) {
+                    $sender->sendMessage(Bedwars::PREFIX."Arena not found! Be sure to register it");
+                    return FALSE;
+                } else {
+                    $sender->sendMessage(Bedwars::PREFIX."Break a Sign");
+                    Bedwars::$players[$sender->getName()]->setPos(-11);
+                    Bedwars::$players[$sender->getName()]->setKnocker($args[0]);
+                    return TRUE;
+                }
+            }
+        }
+        return FALSE;
+    }
 
 }
