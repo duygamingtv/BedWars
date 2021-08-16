90116d1ca37524ceb51cdb7ee4eeac5f703cf327
DRAGKILLS <70339357+DRAGKILLS@users.noreply.github.com>
2021-08-16 08:30:16 -0700

    Update PlayerJoinListener.php

--- src/Fludixx/Bedwars/event/PlayerJoinListener.php
+++ src/Fludixx/Bedwars/event/PlayerJoinListener.php
@@ -18,19 +18,19 @@ use pocketmine\item\Item;
 
 class PlayerJoinListener implements Listener {
 
-	public function onPlayerJoin(PlayerLoginEvent $event) {
-		$event->getPlayer()->teleport(Bedwars::getInstance()->getServer()->getDefaultLevel()->getSafeSpawn());
-		Bedwars::$players[$event->getPlayer()->getName()] = new BWPlayer($event->getPlayer());
-		if(!Bedwars::$statsSystem->isRegistered($event->getPlayer())) {
-		    Bedwars::$statsSystem->register($event->getPlayer());
+    public function onPlayerJoin(PlayerLoginEvent $event) {
+        $event->getPlayer()->teleport(Bedwars::getInstance()->getServer()->getDefaultLevel()->getSafeSpawn());
+        Bedwars::$players[$event->getPlayer()->getName()] = new BWPlayer($event->getPlayer());
+        if(!Bedwars::$statsSystem->isRegistered($event->getPlayer())) {
+            Bedwars::$statsSystem->register($event->getPlayer());
             Bedwars::$statsSystem->set($event->getPlayer(), 'kills', 0);
             Bedwars::$statsSystem->set($event->getPlayer(), 'deaths', 0);
             Bedwars::$statsSystem->set($event->getPlayer(), 'beds', 0);
             Bedwars::$statsSystem->set($event->getPlayer(), ':requests', TRUE);
         }
-		$event->getPlayer()->getInventory()->setContents([
+        $event->getPlayer()->getInventory()->setContents([
             0 => Item::get(Item::IRON_SWORD)
         ]);
-	}
+    }
 
 }
