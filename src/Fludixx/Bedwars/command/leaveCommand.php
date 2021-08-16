2b8f623a60372ed69918761333d1561d698cc04b
DRAGKILLS <70339357+DRAGKILLS@users.noreply.github.com>
2021-08-16 08:32:14 -0700

    done

--- src/Fludixx/Bedwars/command/leaveCommand.php
+++ src/Fludixx/Bedwars/command/leaveCommand.php
@@ -18,27 +18,27 @@ use pocketmine\Player;
 
 class leaveCommand extends Command {
 
-	public function __construct()
-	{
-		parent::__construct("leave",
-			"Teleports you back to the Spawn",
-			"/leave",  ["l"]);
-	}
+    public function __construct()
+    {
+        parent::__construct("leave",
+            "Teleports you back to the Spawn",
+            "/leave",  ["l"]);
+    }
 
-	public function execute(CommandSender $sender, string $commandLabel, array $args)
-	{
-		if($sender instanceof Player) {
-			$player = Bedwars::$players[$sender->getName()];
-			$player->getPlayer()->setGamemode(0);
-			$player->rmScoreboard($sender->getLevel()->getFolderName());
-			$player->saveTeleport(Bedwars::getInstance()->getServer()->getDefaultLevel()->getSafeSpawn());
-			$player->setPos(0);
-			$player->setSpectator(FALSE);
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        if($sender instanceof Player) {
+            $player = Bedwars::$players[$sender->getName()];
+            $player->getPlayer()->setGamemode(0);
+            $player->rmScoreboard($sender->getLevel()->getFolderName());
+            $player->saveTeleport(Bedwars::getInstance()->getServer()->getDefaultLevel()->getSafeSpawn());
+            $player->setPos(0);
+            $player->setSpectator(FALSE);
             $sender->getInventory()->setContents([
                 0 => Item::get(Item::IRON_SWORD)
             ]);
-			$player->getPlayer()->getArmorInventory()->clearAll();
-		}
-	}
+            $player->getPlayer()->getArmorInventory()->clearAll();
+        }
+    }
 
 }
